25d87186a61d35df123f8a14c05bea8109263c04
BaerVervergaert <bgmj.ververgaert@gmail.com>
2022-09-25 12:45:36 +0200

    Minor changes to test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 from FunctionRelations import PartialFunctionRelation, FunctionRelation, SymbolicFunctionRelation
 from FunctionSystem import FunctionSystem, SymbolicFunctionSystem
